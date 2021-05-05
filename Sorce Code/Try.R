674551cdf7bc56e2cb5d69c0ae75a5f6313f85c7
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-05-05 19:20:01 +0200

    new cleaning changes

--- Sorce Code/Try.R
+++ Sorce Code/Try.R
@@ -14,3 +14,62 @@ unique(train['recorded_by'])
 # Since we can see that 'recorded_by' column has no unique elements present so we can drop.
 train <- train[ , !names(train) %in% c('recorded_by')]
 dim(train) # Checking our dataset dimension after dropping a column 
+
+# Checking our second column amount_tsh
+str(train['amount_tsh'])
+table(train$amount_tsh)
+# From the below table we can say that it consist off more zeros so we can drop it
+train <- train[ , !names(train) %in% c('amount_tsh')]
+dim(train) # Checking our dataset dimension
+
+# In public meeting we are removing that column 
+train <- train[ , !names(train) %in% c('public_meeting')]
+dim(train)
+
+# Checking permit column
+table(train$permit)
+# From below table we can see that there is a empty string and we can't take the mean values as it is boolean value
+# So drop that column
+train <- train[ , !names(train) %in% c('permit')]
+dim(train)
+
+# Grouping extraction type,extraction type group and extraction type class
+View(data.frame(train$extraction_type,train$extraction_type_group,train$extraction_type_class))
+# We can say that three column have a common value and hence we can remove one column
+train <- train[ , !names(train) %in% c('extraction_type_group')]
+
+# Grouping management and management_group
+View(data.frame(train$management,train$management_group))
+# We can't drop anything from above mentioned columns as they posses different values
+
+# Compare scheme name and scheme management
+View(data.frame(train$scheme_name,train$scheme_management))
+table(train$scheme_name) # Checking the table of values for scheme_name
+table(train$scheme_management) # Checking the table of values for scheme_management
+# Since scheme_name consist of too many categorical values we can eliminate it
+train <- train[ , !names(train) %in% c('scheme_name')]
+dim(train)
+
+# Comparing the payment and payment_type
+View(train[c('payment','payment_type')])
+# Since we can infer that they both have a close relation so we can eliminate either of the column
+train <- train[ , !names(train) %in% c('payment')]
+dim(train)
+
+# Comparing the water_quality and quality_group
+View(train[c('water_quality','quality_group')])
+# Since we can infer that they both have a close relation so we can eliminate either of the column
+train <- train[ , !names(train) %in% c('water_quality')]
+dim(train)
+
+# Comparing the quantity and quantity_group
+View(train[c('quantity','quantity_group')])
+# Since we can infer that they both have a close relation so we can eliminate either of the column
+train <- train[ , !names(train) %in% c('quantity_group')]
+dim(train)
+
+# Comparing the source_type, source and source_class
+View(train[c('source_type','source','source_class')])
+# By comparing we can say that source_type and source are same so we can drop either of it but source_class defines type of the water source
+train <- train[ , !names(train) %in% c('source_type')]
+dim(train)
