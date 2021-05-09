eaeef2c5cefb56a3b74f1ce75de4e7668a379122
Aneruth Mohanasundaram <ane1998@gmail.com>
2021-05-09 02:10:53 +0200

    cleaning test data

--- Sorce Code/Try.R
+++ Sorce Code/Try.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(dplyr)
 
+
 # Load the dataset
 test <- read.csv('/Users/aneruthmohanasundaram/Documents/GitHub/R-Basics/Machine Learning/Datasets/Test_set_values.csv')
 train <- read.csv('/Users/aneruthmohanasundaram/Documents/GitHub/R-Basics/Machine Learning/Datasets/Training_set_values.csv')
@@ -27,30 +28,50 @@ ggplot(as.data.frame(lab), aes(factor(status_group), Freq)) + geom_col(position 
 unique(train['recorded_by'])
 # Since we can see that 'recorded_by' column has no unique elements present so we can drop.
 train <- train[ , !names(train) %in% c('recorded_by')]
-dim(train) # Checking our dataset dimension after dropping a column 
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('recorded_by')]
+
+# Checking our dataset dimension after dropping a column 
+dim(train) 
 
 # Checking our second column amount_tsh
 str(train['amount_tsh'])
 table(train$amount_tsh)
 # From the below table we can say that it consist off more zeros so we can drop it
 train <- train[ , !names(train) %in% c('amount_tsh')]
-dim(train) # Checking our dataset dimension
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('amount_tsh')]
+
+# dim(train) # Checking our dataset dimension
 
 # In public meeting we are removing that column 
 train <- train[ , !names(train) %in% c('public_meeting')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('public_meeting')]
+
+# dim(train)
 
 # Checking permit column
 table(train$permit)
 # From below table we can see that there is a empty string and we can't take the mean values as it is boolean value
 # So drop that column
 train <- train[ , !names(train) %in% c('permit')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('permit')]
+
+# dim(train)
 
 # Grouping extraction type,extraction type group and extraction type class
 View(data.frame(train$extraction_type,train$extraction_type_group,train$extraction_type_class))
 # We can say that three column have a common value and hence we can remove one column
 train <- train[ , !names(train) %in% c('extraction_type_group')]
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('extraction_type_group')]
 
 ######################################## Get back to it later
 # Grouping management and management_group
@@ -63,54 +84,87 @@ table(train$scheme_name) # Checking the table of values for scheme_name
 table(train$scheme_management) # Checking the table of values for scheme_management
 # Since scheme_name consist of too many categorical values we can eliminate it
 train <- train[ , !names(train) %in% c('scheme_name')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('scheme_name')]
+
+# dim(train)
 
 # Comparing the payment and payment_type
 View(train[c('payment','payment_type')])
 # Since we can infer that they both have a close relation so we can eliminate either of the column
 train <- train[ , !names(train) %in% c('payment')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('payment')]
+
+# dim(train)
 
 # Comparing the water_quality and quality_group
 View(train[c('water_quality','quality_group')])
 # Since we can infer that they both have a close relation so we can eliminate either of the column
 train <- train[ , !names(train) %in% c('quality_group')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('quality_group')]
+
+# dim(train)
 
 # Comparing the quantity and quantity_group
 View(train[c('quantity','quantity_group')])
 # Since we can infer that they both have a close relation so we can eliminate either of the column
 train <- train[ , !names(train) %in% c('quantity_group')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('quantity_group')]
+
+# dim(train)
 
 # Comparing the source_type, source and source_class
 View(train[c('source_type','source','source_class')])
 # By comparing we can say that source_type and source are same so we can drop either of it but source_class defines type of the water source
 train <- train[ , !names(train) %in% c('source_type')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('source_type')]
+
+# dim(train)
 
 # Comparing the source_type, source and source_class
 View(train[c('waterpoint_type','waterpoint_type_group')])
 # By comparing we can say that source_type and source are same so we can drop either of it but source_class defines type of the water source
 train <- train[ , !names(train) %in% c('waterpoint_type_group')]
-dim(train)
+
+# Doing the same for test dataset
+test <- test[ , !names(test) %in% c('waterpoint_type_group')]
+
+# dim(train)
 
 # drop columns
 train <- train[ , !names(train) %in% c('id','wpt_name','num_private','longitude','latitude','lga','ward','amount_tsh','subvillage','region','gps_height','funder','installer')]
+
+# Doing the above for test data
+test <- test[ , !names(test) %in% c('id','wpt_name','num_private','longitude','latitude','lga','ward','amount_tsh','subvillage','region','gps_height','funder','installer')]
+
 dim(train)
+dim(test)
 
 # Converting the date to days
 train$date_recorded = as.numeric(as.Date(Sys.Date()) - as.Date(c(train$date_recorded)))
-View(train)
+test$date_recorded = as.numeric(as.Date(Sys.Date()) - as.Date(c(test$date_recorded)))
+# View(train)
 
 # Replacing all 0 with NA
 train[train == 0]  <- NA
+test[test == 0]  <- NA
 
 # Considering the mean values for NA in population column
 train$population <- ifelse(is.na(train$population), mean(train$population, na.rm=TRUE), train$population)
+test$population <- ifelse(is.na(test$population), mean(test$population, na.rm=TRUE), test$population)
 
 # Considering the mean values for NA in Construction Year column
 train$construction_year <- ifelse(is.na(train$construction_year), mean(train$construction_year, na.rm=TRUE), train$construction_year)
+test$construction_year <- ifelse(is.na(test$construction_year), mean(test$construction_year, na.rm=TRUE), test$construction_year)
 
 # Converting the categorical value of to top 4 categorical values 
 clean_dataset <- function(x){
@@ -139,7 +193,7 @@ clean_dataset <- function(x){
   # Considering the management_group column
   x$management_group <- ifelse(x$management_group != 'user-group' & x$management_group != 'commercial','other_mgtgroup',x$management_group)
   
-  # Considering the payment_type column
+  # Considering the payment column
   x$payment_type <- ifelse(x$payment_type != 'per bucket' & x$payment_type != 'never pay' & x$payment_type != 'monthly','other_payemt',x$payment_type)
   
   # Considering the water_quality column
@@ -160,4 +214,19 @@ clean_dataset <- function(x){
 }
 
 cleaned_train <- clean_dataset(train)
+cleaned_test <- clean_dataset(test)
 View(cleaned_train)
+View(cleaned_test)
+
+
+# Performing one hot encoding
+
+
+
+###################################################### TESTING ###################################################################
+
+
+###################################################### TESTING #################################################################
+
+
+
